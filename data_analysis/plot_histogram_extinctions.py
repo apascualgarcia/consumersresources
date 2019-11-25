a40adce83cc886b7f5b6c6c94939d3ad44f035bd
buleo <buleo@student.ethz.ch>
2019-11-25 19:45:56 +0100

    Added time to reach equilibrium as output of histogram c++ file

---
+++ data_analysis/plot_histogram_extinctions.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+filename = 'histogram_extinctions_nest0.1_conn0.0832'
+data = np.
--- src/CRModel_targets/compute_average_extinctions.cc
+++ src/CRModel_targets/compute_average_extinctions.cc
@@ -9,7 +9,7 @@ int main(int argc, char * argv[]){
   initialize_random_engine(metaparams);
   std::ofstream myfile;
   std::string spath = metaparams.save_path;
-  myfile.open(spath);
+  myfile.open(spath, std::ofstream::out | std::ofstream::trunc);
   if(not(myfile.is_open())){
     std::cerr << "Could not open " << spath << " for writing the extinctions " << std::endl;
   }else{
@@ -18,8 +18,7 @@ int main(int argc, char * argv[]){
       CRModel model(metaparams);
       model.perturb_parameters(metaparams.perturb_parameters);
       Extinction ext = model.evolve_until_equilibrium(1e-6);
-      std::cout << ext.extinct << " " ;
-      myfile << ext.extinct << " ";
+      myfile  << ext.extinct << " " << ext.t_eq << std::endl;
     }
     myfile << std::endl;
   }
