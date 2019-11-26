8658b6d9765408d93e987a4a76617d441abc4a77
buleo <buleo@student.ethz.ch>
2019-11-27 00:21:49 +0100

    Script should finally work now

--- additional_scripts/run_comparison_metaparams.py
+++ additional_scripts/run_comparison_metaparams.py
@@ -4,12 +4,12 @@ from datetime import datetime
 from separate_file import separate_file, remove_files
 
 config_file = sys.argv[1]
-matrix_list = sys.argv[2]
+m_list = sys.argv[2]
 cores = int(sys.argv[3])
 
 with open('./config/' + config_file + '.in') as f:
     mylist = f.read().splitlines()
-matrix_list = separate_file(matrix_list, cores)
+matrix_list = separate_file(m_list, cores)
 seed_number = 0
 for i in range(0, cores):
     cmd_core = 'nohup '
@@ -19,7 +19,7 @@ for i in range(0, cores):
         cmd_core += " path_to_food_matrix=" + \
             matrix_list[i]
         cmd_core += " path_to_save_file=./data_output/" + \
-            config + "_" + str(i) + ".out"
+            config + "_" + m_list + "_" + str(i) + ".out"
         cmd_core += " seed_number=" + str(seed_number)
         cmd_core += " | ts '[%Y-%m-%d %H:%M:%S]' > ./logs/" + \
             config + "_" + str(i) + ".log"
@@ -28,7 +28,7 @@ for i in range(0, cores):
         seed_number += 1
     cmd_core = cmd_core[:-3]
     cmd_core = cmd_core + '&'
-    print(cmd_core)
+    os.system(cmd_core)
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
     cmd_write = '[' + dt_string + '] ' + cmd_core
