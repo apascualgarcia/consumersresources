1e795f72a28713aa0f14e569dd5696bf309f6552
buleo <buleo@student.ethz.ch>
2019-11-26 17:46:14 +0100

    Added a script which lets the user choose on how many cores they want to run

--- additional_scripts/run_comparison_metaparams.py
+++ additional_scripts/run_comparison_metaparams.py
@@ -1,18 +1,34 @@
 import os
-config_file = 'list_of_config_files_comparison'
-matrix_list = 'matrix_list_NR25_NS25'
-cores = 6
+import sys
+from separate_file import separate_file, remove_files
+
+config_file = sys.argv[1]
+matrix_list = sys.argv[2]
+cores = int(sys.argv[3])
+
 with open('./config/' + config_file + '.in') as f:
     mylist = f.read().splitlines()
 
-for config in mylist:
-    for i in range(1, cores + 1):
-        cmd = 'nohup ./build/compute_critical_Delta_matrices '
-        cmd += 'config/' + config + '.in'
-        cmd += " path_to_food_matrix=./config/" + \
+conf_files = separate_file(config_file, cores)
+
+seed_number = 0
+for i in range(0, cores):
+    cmd_core = 'nohup '
+    for config in mylist:
+        cmd_core += './build/compute_critical_Delta_matrices '
+        cmd_core += 'config/' + config + '.in'
+        cmd_core += " path_to_food_matrix=./config/" + \
             matrix_list + "_" + str(i) + '.in'
-        cmd += " path_to_save_file=./data_output/" + \
+        cmd_core += " path_to_save_file=./data_output/" + \
             config + "_" + str(i) + ".out"
-        cmd += " > ./logs/" + config + "_" + str(i) + ".log"
-        cmd += " 2>./logs/err" + config + "_" + str(i) + ".log &"
-        os.cmd(cmd)
+        cmd_core += " seed_number=" + str(seed_number)
+        cmd_core += " |ts '[%Y-%m-%d %H:%M:%S]' > ./logs/" + \
+            config + "_" + str(i) + ".log"
+        cmd_core += " 2>./logs/err" + config + "_" + \
+            str(i) + ".log && "
+        seed_number += 1
+    cmd_core = cmd_core[:-3]
+    cmd_core = cmd_core + '&'
+    os.system(cmd_core)
+print("Launched the commands in the background, please check the appropriate logs to see the progress")
+remove_files(conf_files)
--- additional_scripts/separate_file.py
+++ additional_scripts/separate_file.py
@@ -1,18 +1,14 @@
 import os
 import numpy as np
 
-config_folder = './config'
-config_file = 'matrix_list_NR25_NS25'
-CORES = 6
 
-
-def separate_file(conf_file):
-    with open(conf_file) as f:
+def separate_file(conf_file, CORES):
+    with open('config/' + conf_file + '.in') as f:
         mylist = f.read().splitlines()
     indices_tables = np.array_split(np.arange(0, len(mylist)), CORES)
     filenames = []
     for i in range(len(indices_tables)):
-        filenames.append('./config/' + config_file + '_' + str(i) + '.in')
+        filenames.append('./config/' + conf_file + '_' + str(i) + '.in')
         f = open(filenames[i], 'w+')
         for j in indices_tables[i]:
             f.write(str(mylist[j]) + '\n')
@@ -24,6 +20,3 @@ def remove_files(mylist):
     for file in mylist:
         cmd = "rm " + file
         os.system(cmd)
-
-
-config_files = separate_file(config_folder + '/' + config_file + '.in')
