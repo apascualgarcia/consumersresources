0f53801b8ba44382ddacb9ad8917d646d4aef006
buleo <buleo@student.ethz.ch>
2020-01-14 15:20:23 +0100

    Modified run_comparison_metaparams.py (error logs did not work)

--- additional_scripts/run_comparison_metaparams.py
+++ additional_scripts/run_comparison_metaparams.py
@@ -3,37 +3,37 @@ import sys
 from datetime import datetime
 from separate_file import separate_file, remove_files
 
+
 config_file = sys.argv[1]
 m_list = sys.argv[2]
 cores = int(sys.argv[3])
-command = sys.argv[4]
+command_name = sys.argv[4]
 
 with open('./config/' + config_file + '.in') as f:
     mylist = f.read().splitlines()
 matrix_list = separate_file(m_list, cores)
 seed_number = 0
 for i in range(0, cores):
-    cmd_core = 'nohup '
+    cmd_core = 'nohup sh -c "'
     for config in mylist:
-        cmd_core += (command + ' ')
+        cmd_core += (command_name + ' ')
         cmd_core += 'config/' + config + '.in'
         cmd_core += " path_to_food_matrix=" + \
             matrix_list[i]
         cmd_core += " path_to_save_file=./data_output/" + \
             config + "_" + m_list + "_" + str(i) + ".out"
         cmd_core += " seed_number=" + str(seed_number)
-        cmd_core += " | ts '[%Y-%m-%d %H:%M:%S]' > ./logs/" + \
+        cmd_core += '| ts [%Y-%m-%d %H:%M:%S]"> ./logs/' + \
             config + "_" + m_list + "_" + str(i) + ".log"
-        cmd_core += " 2>./logs/err" + config + "_" + \
-            str(i) + ".log && "
+        cmd_core += " 2>./logs/err" + config + "_" + m_list + '_'+\
+            str(i) + '.log '
         seed_number += 1
-    cmd_core = cmd_core[:-3]
-    cmd_core = cmd_core + '&'
+    #cmd_core = cmd_core[:-3]
+    cmd_core += '&'
     os.system(cmd_core)
     now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    dt_string = now.strftime('%d/%m/%Y %H:%M:%S')
     cmd_write = '[' + dt_string + '] ' + cmd_core
-    cmd_write = 'echo "' + cmd_write + '" >> logs/commands.log'
-    cmd_write += '& echo $! > /tmp/pid'
-    #os.system(cmd_write)
+    cmd_write = "echo '" + cmd_write + "' >> logs/commands.log"
+    os.system(cmd_write)
 print("Launched the commands in the background, please check the appropriate logs to see the progress")
