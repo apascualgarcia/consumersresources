4a55986ade9981be83aef57dc993c43761e5c996
buleo <buleo@student.ethz.ch>
2020-02-09 18:21:51 +0100

    Added writing to external file

--- src/CRModel_targets/test_feasible_alpha_max.cc
+++ src/CRModel_targets/test_feasible_alpha_max.cc
@@ -6,12 +6,16 @@ int main(int argc, char * argv[]){
     ntype eps_min=0.1, eps_max=0.25;
     unsigned int eps_points=10;
 
+    std::ofstream myfile = open_external_file_truncate(metaparams.save_path);
+
     nvector eps_interval = linear_interval(eps_min, eps_max, eps_points);
     for(size_t i=0; i < eps_points; ++i){
       metaparams.epsilon=eps_interval[i];
       std::cout << "For epsilon=" << metaparams.epsilon << ", maximum feasible alpha0 is ";
       std::cout << metaparams.feasible_alpha_max() << std::endl;
+      myfile << metaparams.epsilon << " " << metaparams.feasible_alpha_max() << std::endl;
 
+    myfile.close();
     }
 
 
