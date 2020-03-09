b5358a7506725f767ca4342831f5497e303602d0
buleo <buleo@student.ethz.ch>
2020-03-09 15:56:55 +0100

    Corrected bug in optimize matrices which did not get the right initial temperature when changing matrices

--- src/CRModel_targets/optimize_matrices.cc
+++ src/CRModel_targets/optimize_matrices.cc
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]){
     std::vector<std::string> matrices_list=load_food_matrix_list(metaparams.foodmatrixpath);
     std::cout<< matrices_list << std::endl;
     MonteCarloSolver mcsolv;
-    mcsolv.T=10.;
+    ntype T0;
     mcsolv.max_steps=1000000;
     mcsolv.max_fails=1000;
     mcsolv.annealing_freq=1000;
@@ -30,6 +30,7 @@ int main(int argc, char* argv[]){
       std::ofstream myfile=open_external_file_truncate(metaparams.save_path+"_other");
 
       std::cout << "Starting the Monte Carlo algorithm to find the optimal syntrophy matrix ... " << std::endl;
+      mcsolv.T=10.;
       foodmatrix optimal_alpha=optimal_syntrophy_from_consumption(gamma,coprophagy, mcsolv);
       display_food_matrix(myfile, optimal_alpha);
       std::cout << "An optimal syntrophy matrix was found and saved in " << metaparams.save_path << std::endl;
