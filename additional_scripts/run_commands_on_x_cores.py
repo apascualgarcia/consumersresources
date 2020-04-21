3d4f88ec0460e358d288322b6416027dc7642820
buleo <buleo@student.ethz.ch>
2020-04-21 20:56:57 +0200

    I hope it's finally gonna work

--- additional_scripts/run_commands_on_x_cores.py
+++ additional_scripts/run_commands_on_x_cores.py
@@ -12,13 +12,13 @@ commands = np.array([" ".join(f) for f in np.loadtxt(file_name, dtype='U')])
 commands_per_core = np.array_split(commands, CORES)
 
 for i in range(CORES):
-    core_command = ''
+    core_command = '('
     for j in range(len(commands_per_core[i])):
         split_files = commands_per_core[i][j].split(">")
         command = split_files[0]
         log_file = split_files[1][:-1]
         err_file = split_files[2]
         core_command+='nohup sh -c "'+command+'">'+log_file[1:]+"2>"+err_file+"; wait;"
-    core_command=core_command[:-7]+' &'
+    core_command=core_command[:-7]+') &'
     print(core_command)
     os.system(core_command)
