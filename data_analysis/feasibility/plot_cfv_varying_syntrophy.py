86f3de9c9a111e90e82ad3e4d5f2568b141c752e
buleo <buleo@student.ethz.ch>
2020-05-11 23:38:52 +0200

    Updated changes of the day

--- data_analysis/feasibility/plot_cfv_varying_syntrophy.py
+++ data_analysis/feasibility/plot_cfv_varying_syntrophy.py
@@ -15,6 +15,13 @@ optimal_LRI_folder='optimal_LRI_Nr25_Nc25'
 consumption_matrix_folder='optimal_matrices/consumption/Nr25_Nc25'
 matrix_set='S_{25}'
 
+# filename = 'feasibility/feasibility_NR50_NS25_full_rank_opt_consumption_mat_NR50_NS25'
+# optimal_LRI_folder='optimal_LRI_Nr50_Nc25'
+# consumption_matrix_folder='optimal_matrices/consumption/Nr50_Nc25'
+# matrix_set='S_{50}'
+
+
+
 cmap = plt.cm.get_cmap('jet_r')
 colors = [cmap(i/10) for i in range(len(alpha0))]
 
