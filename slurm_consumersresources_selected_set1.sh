3918240652fe56ea41c14529ec1a96ab80b06f1d
apascual <alberto.pascual.garcia@gmail.com>
2025-12-30 18:10:45 +0100

    prepared run for selected opt matrix in 4 sets elbrus

---
+++ slurm_consumersresources_selected_set1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J crm_sel_set1
+#SBATCH -o output_file.output
+#SBATCH -e error.errors
+#SBATCH --mem=25000
+#SBATCH --time=13-1:1:1
+#SBATCH --cpus-per-task=30
+
+# Example with a job called myjob, with the output redirected to output_file.output
+# 50000 Mb to be used, 7 days 1 hour 1 minute and 1 second as maximum execution time.
+# python plasticmix_simulation.py > output_mixplastic_second.txt # Execution line, chage it!
+./run_commands commands/study_systems_selected_set1.txt 30
